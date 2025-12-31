baf6d0c18ec1b5704c736e66512e2bdf613effd2
Avery Davis <averydavis9@proton.me>
2025-12-31 14:53:09 -0800

    jquery & custom css + js directories

---
+++ static/custom-js/home.js
@@ -0,0 +1,5 @@
+  $(document).ready(function() {
+    // jQuery test: Change the color of the test text
+    $('#jquery-test').css('color', 'red').css('font-weight', 'bold');
+    console.log('jQuery is working!');
+  });
